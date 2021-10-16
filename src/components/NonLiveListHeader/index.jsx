181565939c8f7c7c712e3f0678fab97aedd6e60d
Rawan96 <rawan.designer@hotmail.com>
2021-10-16 13:45:04 +0300

    Create the nonLive list page

--- src/components/NonLiveListHeader/index.jsx
+++ src/components/NonLiveListHeader/index.jsx
@@ -5,24 +5,26 @@ import './style.css';
 
 const NonLiveListHeader = () => (
   <>
-    <div className='nonLive-head-container'>
-      <p className='nonLive-header'>
-        NONLIVE
-      </p>
-      <p className='nonLive-header-para'>
-        실시이브경기
-      </p>
-      <p className='nonLive-header width'>
-        80
-      </p>
-      <img
-        style={{
-          backgroundImage: { background },
-          backgroundRepeat: 'repeat',
-        }}
-        src={image}
-        alt='dropdown'
-      />
+
+    <div className='nonLive-header-wrapper'>
+      <div className='nonLive-head-container'>
+        <p className='nonLive-header'>
+          NONLIVE
+        </p>
+        <p className='nonLive-header-para'>
+          실시이브경기
+        </p>
+        <p className='nonLive-header width'>
+          80
+        </p>
+        <img
+          className='nonLive-header-img'
+          src={image}
+          alt='dropdown'
+        />
+
+      </div>
+
     </div>
   </>
 
