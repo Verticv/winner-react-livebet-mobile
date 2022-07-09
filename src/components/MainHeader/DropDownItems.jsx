53593e0db2215b37e80c7d050092c21f13e2e778
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-07-09 04:49:24 +0300

    edit dropdown button (fix point 13)

--- src/components/MainHeader/DropDownItems.jsx
+++ src/components/MainHeader/DropDownItems.jsx
@@ -1,66 +1,56 @@
-import React, { useEffect, useState } from "react";
-import checkIcon from '../../assets/images/imgs/true.png';
-import btnArrow from '../../assets/images/imgs/btn-arrow.png';
-import arrow from '../../assets/images/imgs/aaa.png'
+import React, { useState } from "react";
+import arrow from '../../assets/images/imgs/btn-arrow.png';
+import icon from '../../assets/images/imgs/true.png';
 
-export default function SelectBox({ items }) {
-    const [showItems, setShowItems] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(items[0]);
-    useEffect(() => {
-        const closeDropdown = (e) => {
-            if ((e.path[0].className !== 'select-box--selected-item')
-                && e.path[0].tagName !== 'IMG'
-                && e.path[0].tagName !== 'P') {
-                setShowItems(false);
-            };
-        };
-        document.body.addEventListener('click', closeDropdown);
-        return () => document.body.addEventListener('click', closeDropdown);
-    }, []);
+const SortBy = ({ children, options, value, onChange, onClick, multiple }) => (
+    <div className="container" style={{ padding: '0' }}>
+        <select
+            className="select-overlay"
+            id="filter-select"
+            name="filter-select[]"
+            value={value}
+            onChange={onChange}
+            onClick={onClick}
+            multiple={multiple}
+        >
+            {options.map(option => (
+                <option key={option} value={option}>
+                    {option}
+                </option>
+            ))}
+        </select>
+        <label style={{ fontSize: '2.625rem' }} className="custom" htmlFor="filter-select">
+            {children}
+        </label>
+    </div>
+);
 
-    const dropDown = () => {
-        setShowItems((prevState) => !prevState);
-    };
+const Dropdown = ({ options, children, labelStyle, labelClasses, isLeagueSelection = false, isContact = false, multiple = false, isSignup = false }) => {
+    const [sortValue, setSortValue] = useState(isContact ? "선택" : isLeagueSelection ? "리그선택" : isSignup ? options[0] : "전체회차")
 
-    const selectItem = (event, item) => {
-        event.stopPropagation();
-        setSelectedItem(item);
-        setShowItems(false);
-    };
+    const onChange = e => setSortValue(e.currentTarget.value);
 
     return (
-        <div className="select-box--box">
-            <div className="select-box--container btn">
-                <div onClick={dropDown} className="select-box--selected-item">
-                    <img 
-                    // style={{width: '4.125rem', height: '3.25rem'}} 
-                    className="icon" src={checkIcon} alt="check" />
-                    <p className="text">{selectedItem.value}</p>
-                    <img  className='white-arrow' 
-                    // style={{ width: '2rem', height: '1rem', marginLeft: '1rem', marginTop: '1rem' }} 
-                    src={showItems ? arrow : btnArrow} alt=''/>
-                    <div
-                        className={`select-box--arrow ${showItems ? "select-box--arrow-up" : ""
-                            }`}
-                    >
-                    </div>
+        <form style={{ width: '100%' }}>
+            <SortBy
+                options={options}
+                value={sortValue}
+                onChange={onChange}
+                onClick={() => console.log('on click!')}
+                multiple={multiple}
+            >
+                <div className="flex w-full items-center justify-between">
+                    <img style={{ width: '3.3125rem', height: '3.1rem' }} src={icon} alt="icon" />
+                    <div className={labelClasses} style={labelStyle}><p style={{ marginTop: "2.5rem" }}>{sortValue}</p></div>
+                    <img style={{ width: '1.375rem', height: '0.6875rem' }} src={arrow} alt="arrow" />
                 </div>
-                <div
-                    style={{ display: showItems ? "block" : "none" }}
-                    className={`select-box--items`}
-                >
-                    {items.map((item) => (
-                        <div
-                            key={item.id}
-                            onClick={(event) => selectItem(event, item)}
-                            className={`${selectedItem === item ? "selected" : ""
-                                } select-box--item`}
-                        >
-                            <p>{item.value}</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
+            </SortBy>
+        </form>
+    )
 }
+
+
+export default Dropdown;
+
+
+
--- src/components/MainHeader/index.jsx
+++ src/components/MainHeader/index.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useRef, useLayoutEffect } from 'react';
-// import cup from '../../assets/images/cup/cup-gold.png';
-// import chronology from '../../assets/images/chronology/chronology-white.png';
+import React, { useState } from 'react';
+import Dropdown from './DropDownItems';
 import chronology from '../../assets/images/imgs/ee3.png';
 import cup from '../../assets/images/imgs/ee1.png';
 import Modal from '../Modal';
@@ -8,33 +7,20 @@ import useModal from '../../hooks/useModal';
 import btnArrow from '../../assets/images/imgs/btn-arrow.png';
 import cup2 from '../../assets/images/imgs/ee2.png';
 
-
 const MainHeader = () => {
   const { toggle, visible } = useModal();
-  const selectRef = useRef(null);
-  const selectContainerRef = useRef(null);
-  const [width, setWidth] = useState('13.6rem');
   const [isSelected, setIsSelected] = useState(true);
   const [isCheck, setIsCheck] = useState([]);
   const [temp, setTemp] = useState([]);
   const [isClick, setIsClick] = useState(false);
-  const [change, setChange] = useState(false);
   const changeName = () => {
     setIsSelected((prev) => !prev);
   };
-  // useLayoutEffect(() => {
-  //   setWidth(selectRef.current.offsetWidth);
-  //   // setHeight(ref.current.offsetHeight);
-  // }, []);
-  useLayoutEffect(() => {
-    const html = document.querySelector('html');
-    const fontSize = html.style.fontSize.split('p')[0];
-    // console.log('html.style.fontSize', html.style.fontSize.split('p')[0]);
-    selectContainerRef.current.width = `${(selectRef.current.offsetWidth + (3.3125 * fontSize) + (1.375 * fontSize)) / fontSize}rem`;
-    // console.log(`${(selectRef.current.offsetWidth + (3.3125 * fontSize) + (1.375 * fontSize)) / 16} rem`);
-    // setWidth(`${(selectRef.current.offsetWidth + (3.3125 * fontSize) + (1.375 * fontSize)) / fontSize}rem`);
-    setWidth(selectContainerRef.current.width);
-  }, [change]);
+  const banksOptions = [
+    "전체",
+    "진행중",
+    "예정중",
+  ];
   return (
     <>
       <div className='container-main-header'>
@@ -72,14 +58,8 @@ const MainHeader = () => {
               <img style={{ marginRight: '0.3rem', marginTop: '0.4rem' }} className='white-arrow' src={btnArrow} alt='' />
             </div>
           </button>
-          <div className="select" ref={selectContainerRef}>
-            <select className="btn" ref={selectRef}
-              style={{ width: width }}
-              onChange={() => setChange(!change)}>
-              <option value="전체">전체</option>
-              <option value="진행중">진행중</option>
-              <option value="예정중">예정중</option>
-            </select>
+          <div className='dropdown'>
+            <Dropdown options={banksOptions} isSignup={true} />
           </div>
         </div>
       </div>
