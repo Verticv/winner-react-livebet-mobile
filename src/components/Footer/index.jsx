144b3d6c8785eb11bad930321c798ebb0e16066a
MohammedAlghazali <mohammed.m.alghazali@hotmail.com>
2022-07-10 16:08:26 +0300

    fix footer on ios

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import cart from '../../assets/images/other/cart.png';
+import { isIOS } from 'react-device-detect';
 
 const Footer = () => {
+  useEffect(() => {
+    if (isIOS) {
+      console.log('isIOS', isIOS);
+    }
+  })
   const history = useHistory();
 
   const handleClick = () => {
@@ -18,27 +24,27 @@ const Footer = () => {
         <footer className='flex'>
           <div className='sub-container1'>
             <img src={cart} alt='cart' />
-            <p className='main-p' 
+            <p className='main-p'
             // style={{marginLeft: '3rem'}}
             >베팅카드 열기</p>
           </div>
 
           {/* <div className='vl' /> */}
 
-          <div className='sub-container2'>
-            <p 
+          <div className='sub-container2' style={{ paddingRight: `${isIOS ? '4.5rem' : '7rem' } ` }}>
+            <p
             // style={{marginRight: '2rem'}}
             >
               선택경기
-                <span 
-              style={{marginLeft: '2rem', marginRight: '1rem'}}
+              <span
+                style={{ marginLeft: '2rem', marginRight: '1rem' }}
               >3<span className='noto-sans'>건</span></span>
             </p>
             <p className='sub-p'>
               예상배당
               {' '}
-              <span 
-              style={{marginLeft: '1.3rem'}}
+              <span
+                style={{ marginLeft: '1.3rem' }}
               >4.25</span>
             </p>
           </div>
