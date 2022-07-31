6a2fd898fbd18ab2dd8340a75b911dd268ee5091
Andrew Jang <dhj9817@gmail.com>
2022-07-31 17:44:40 +0900

    .

--- src/components/LiveCard/index.jsx
+++ src/components/LiveCard/index.jsx
@@ -94,8 +94,8 @@ function LiveCard({
                       </div>
                     </div>
                     <div className='play-video' style={{marginLeft: isIOS ? '-0.3rem' : '0.5rem'}}>
-                      <p className='livee' style={{padding:  isIOS ? '0.2rem 1.18rem' : '0.2rem 1.19rem'}}>live</p>
-                      <p className='score' style={{marginLeft: isIOS ? '2rem' : '1.88rem'}}>
+                      <p className='livee' style={{padding:  isIOS ? '0.2rem 1.1rem' : '0.2rem 1.19rem'}}>live</p>
+                      <p className='score' style={{marginLeft: isIOS ? '1.8rem' : '1.88rem'}}>
                         {firstScore}
                         <span style={{ color: '#a0a0a0' }}>-</span>
                         {secondScore}
